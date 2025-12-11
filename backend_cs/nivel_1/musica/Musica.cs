d83d0ff5f561fa299a4779a0a7e726e31f30c614
Fábio Henrique <psifabiohenrique@outlook.com>
2025-12-10 23:51:28 -0300

    Configurando a visibilidade e trabalhando com getters e setters.

--- backend_cs/nivel_1/musica/Musica.cs
+++ backend_cs/nivel_1/musica/Musica.cs
@@ -1,16 +1,17 @@
 class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string nome { get; set; }
+    public string artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A musica {this.nome} pertence ao artista {this.artista}";
 
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {this.nome}");
         Console.WriteLine($"Artista: {this.artista}");
-        Console.WriteLine($"duracao: {this.duracao}");
-        if (this.disponivel)
+        Console.WriteLine($"duracao: {this.Duracao}");
+        if (this.Disponivel)
         {
             Console.WriteLine("Disponível no plano.");
         }
--- backend_cs/nivel_1/musica/Program.cs
+++ backend_cs/nivel_1/musica/Program.cs
@@ -1,7 +1,7 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "Roxane";
 musica1.artista = "The Police";
-musica1.duracao = 263;
-musica1.disponivel = true;
+musica1.Duracao = 263;
+musica1.Disponivel = true;
 
 musica1.ExibirFichaTecnica();
