2e1eae961fe09a2800b311f57a3037caaf518fca
Fábio Henrique <psifabiohenrique@outlook.com>
2025-12-10 23:36:57 -0300

    Adicionado o metodo exibir ficha tecnica à classe musica

--- backend_cs/nivel_1/musica/Musica.cs
+++ backend_cs/nivel_1/musica/Musica.cs
@@ -4,4 +4,19 @@ class Musica
     public string artista;
     public int duracao;
     public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {this.nome}");
+        Console.WriteLine($"Artista: {this.artista}");
+        Console.WriteLine($"duracao: {this.duracao}");
+        if (this.disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adiquira o plano Plus");
+        }
+    }
 }
--- backend_cs/nivel_1/musica/Program.cs
+++ backend_cs/nivel_1/musica/Program.cs
@@ -1,3 +1,7 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "Roxane";
 musica1.artista = "The Police";
+musica1.duracao = 263;
+musica1.disponivel = true;
+
+musica1.ExibirFichaTecnica();
