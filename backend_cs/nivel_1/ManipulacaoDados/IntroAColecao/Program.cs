2aac45797a419fb8e3ce2d1b2dadd4fb2ef03e4f
Fábio Henrique <psifabiohenrique@outlook.com>
2025-12-12 12:54:55 -0300

    Código de uma breve revisão de coleções.

---
+++ backend_cs/nivel_1/ManipulacaoDados/IntroAColecao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+string[] diasDaSemana = { "Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+
+List<Produto> produtos =
+[
+    new Produto() { Nome = "Caneta", Preco = 3.45 },
+    new Produto() { Nome = "Caderno", Preco = 23.90 },
+    new Produto() { Nome = "Borracha", Preco = 1.50 }
+];
+
+class Produto {
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+}
