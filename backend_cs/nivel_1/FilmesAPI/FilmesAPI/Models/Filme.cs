33558bf690b9e2e7cb83d825eb59c6d73a985bab
Fábio Henrique <psifabiohenrique@outlook.com>
2025-12-14 20:47:50 -0300

    Adicionado relacionamento 1toMany entre sessoes e filmes

--- backend_cs/nivel_1/FilmesAPI/FilmesAPI/Models/Filme.cs
+++ backend_cs/nivel_1/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -18,4 +18,5 @@ public class Filme
     [Required(ErrorMessage = "A duração do filme é obrigatória")]
     [Range(70, 600, ErrorMessage = "A duração do filme deve ser entre 70 e 600 minutos")]
     public int Duracao { get; set; }
+    public virtual ICollection<Sessao> Sessoes { get; set; }
 }
