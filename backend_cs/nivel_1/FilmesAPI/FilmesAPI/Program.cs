0f2bf9385a0ec96799fcc70b897aed34c39d999d
Fábio Henrique <psifabiohenrique@outlook.com>
2025-12-14 09:06:52 -0300

    Adicionado o swagger ao projeto

--- backend_cs/nivel_1/FilmesAPI/FilmesAPI/Program.cs
+++ backend_cs/nivel_1/FilmesAPI/FilmesAPI/Program.cs
@@ -16,12 +16,22 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Adiciona o Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = String.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
