5a6542853016ca9ae82bfb75e9d0550ba64ab94d
Fábio Henrique <psifabiohenrique@outlook.com>
2025-12-16 10:29:20 -0300

    Adicionado sistema para login de usuário.

--- backend_cs/nivel_1/UsuariosApi/UsuariosApi/Controllers/UsuarioController.cs
+++ backend_cs/nivel_1/UsuariosApi/UsuariosApi/Controllers/UsuarioController.cs
@@ -11,18 +11,25 @@ namespace UsuariosApi.Controllers;
 [Route("[Controller]")]
 public class UsuarioController : ControllerBase
 {
-    private CadastroService _cadastroService;
+    private UsuarioService _usuarioService;
 
-    public UsuarioController(CadastroService cadastroService)
+    public UsuarioController(UsuarioService usuarioService)
     {
-        _cadastroService = cadastroService;
+        _usuarioService = usuarioService;
     }
 
-    [HttpPost]
+    [HttpPost("cadastro")]
     public async Task<IActionResult> CadastroUsuario(CreateUsuarioDto dto)
     {
 
-        await _cadastroService.Cadastra(dto);
+        await _usuarioService.Cadastra(dto);
         return Ok("Usuário cadastrado!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginUsuarioDto dto)
+    {
+        string token = await _usuarioService.Login(dto);
+        return Ok(token);
     }
 }
